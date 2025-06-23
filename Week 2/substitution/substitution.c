97d6c969051d7d2716fa174562d26b9a6d48c59e
Gabriel Rassi <gabriel.rassi.dev@gmail.com>
2025-06-23 11:15:11 -0300

    Update substitution.c

--- Week 2/substitution/substitution.c
+++ Week 2/substitution/substitution.c
@@ -1,1 +1,92 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+bool valid_key(string key);
+bool has_duplicates(string key);
+
+int main(int argc, string argv[])
+{
+    // Validate argument count
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    string key = argv[1];
+
+    // Validate key
+    if (!valid_key(key))
+    {
+        printf("Key must contain 26 unique alphabetic characters.\n");
+        return 1;
+    }
+
+    // Get plaintext from user
+    string plaintext = get_string("plaintext: ");
+
+    printf ("ciphertext: ");
+
+    // Encrypt character by character
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        char c = plaintext[i];
+
+        if (isupper(c))
+        {
+            int index = c - 'A';
+            printf("%c", toupper(key[index]));
+        }
+        else if (islower(c))
+        {
+            int index = c - 'a';
+            printf("%c", tolower(key[index]));
+        }
+        else
+        {
+            printf("%c", c);
+        }
+    }
+
+    printf("\n");
+    return 0;
+
+}
+
+// Validate if key has 26 alphabetic characters and no duplicates
+bool valid_key(string key)
+{
+    if (strlen(key) != 26)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            return false;
+        }
+    }
+
+    return !has_duplicates(key);
+}
+
+// Check for duplicates letters in the key
+bool has_duplicates(string key)
+{
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (tolower(key[i]) == tolower(key[j]))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
